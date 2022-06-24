44032787acdf1ae60dac178c165d2c8367dbd02a
Shrey Suri <shreysuri5@gmail.com>
2022-06-24 12:17:31 -0500

    almost done

--- Base_Translator_2.py
+++ Base_Translator_2.py
@@ -50,6 +50,72 @@ while valid_string == True and i < length:
         valid_string = False
     i = i + 1
 
+if valid_string == True:
+    if base > 9:
+        for i in range (0, length):
+            if num_original[i] == "a":
+                num_original[i] = "10"
+            elif num_original[i] == "b":
+                num_original[i] = "11"
+            elif num_original[i] == "c":
+                num_original[i] = "12"
+            elif num_original[i] == "d":
+                num_original[i] = "13"
+            elif num_original[i] == "e":
+                num_original[i] = "14"
+            elif num_original[i] == "f":
+                num_original[i] = "15"
+            elif num_original[i] == "g":
+                num_original[i] = "16"
+            elif num_original[i] == "h":
+                num_original[i] = "17"
+            elif num_original[i] == "i":
+                num_original[i] = "18"
+            elif num_original[i] == "j":
+                num_original[i] = "19"
+            elif num_original[i] == "k":
+                num_original[i] = "20"
+            elif num_original[i] == "l":
+                num_original[i] = "21"
+            elif num_original[i] == "m":
+                num_original[i] = "22"
+            elif num_original[i] == "n":
+                num_original[i] = "23"
+            elif num_original[i] == "o":
+                num_original[i] = "24"
+            elif num_original[i] == "p":
+                num_original[i] = "25"
+            elif num_original[i] == "q":
+                num_original[i] = "26"
+            elif num_original[i] == "r":
+                num_original[i] = "27"
+            elif num_original[i] == "s":
+                num_original[i] = "28"
+            elif num_original[i] == "t":
+                num_original[i] = "29"
+            elif num_original[i] == "u":
+                num_original[i] = "30"
+            elif num_original[i] == "v":
+                num_original[i] = "31"
+            elif num_original[i] == "w":
+                num_original[i] = "32"
+            elif num_original[i] == "x":
+                num_original[i] = "33"
+            elif num_original[i] == "y":
+                num_original[i] = "34"
+            elif num_original[i] == "z":
+                num_original[i] = "35"
+            else:
+                toggle = False
+    else:
+        toggle = True
+
+    for i in range (0, length):
+        x = int(num_original[i])
+        num_original[i] = x
+
+
+
 
             
             
