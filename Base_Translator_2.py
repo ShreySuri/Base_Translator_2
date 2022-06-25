8d32c00fb48eaecb6fbd97eec2b2c6336f93d0a8
Shrey Suri <shreysuri5@gmail.com>
2022-06-25 17:51:45 -0500

    done!

--- Base_Translator_2.py
+++ Base_Translator_2.py
@@ -1,3 +1,6 @@
+import time
+
+
 def list_len(list_1):
     list_1.append("placeholder")
     i = 0
@@ -11,20 +14,37 @@ def convert(string):
     list1[:0]=string
     return list1
 
-base = 0.5
-while base % 1 != 0 or base < 2 or base > 36:
-    base = input(print("Of what base is the integer? "))
-    base = float(base)
-base = int(base)
-
-
-num_original = input(print("What integer would you like to convert to base 10? Letters should be entered as lowercase." ))
-num_original = convert(num_original)
-
 
 char_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 acceptable_char_list = []
 valid_string = True
+
+
+base = 0.5
+while base % 1 != 0 or base < 2 or base > 36:
+    base = input(print("Please enter the base of the integer. "))
+    print("")
+    base = float(base)
+base = int(base)
+
+num_original = input(print("What integer would you like to convert to base 10? If you are unsure how to enter this string, please type 'map'. "))
+
+if num_original == "map":
+
+    for i in range (0, base):
+        x = char_list[i]
+        acceptable_char_list.append(x)
+
+    print("Character --> Value")
+    for i in range (0, base):
+        x = acceptable_char_list[i]
+        print("%s --> %s" % (x, i))
+    print("")
+    print("Please reload the program and try again.")
+    time.sleep(3600)
+        
+else:
+    num_original = convert(num_original)
 
 
 for i in range (0, base):
